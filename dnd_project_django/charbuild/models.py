b9445bf6ebdecc1e43b84bb17ff2d7cde2f46d12
DESKTOP-VCVB55K\artem <artemskitenko93@gmail.com>
2018-06-20 11:39:50 -0500

    changed Character's model time stamp type from datetime to timezone

--- dnd_project_django/charbuild/models.py
+++ dnd_project_django/charbuild/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from datetime import datetime
+from django.utils import timezone
 
 # Create your models here.
 
@@ -7,7 +7,7 @@ from datetime import datetime
 class Character(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
-    created_at = models.DateTimeField(default=datetime.now, blank=True)
+    created_at = models.DateTimeField(default=timezone.now, blank=True)
 
     def __str__(self):
         return self.title
